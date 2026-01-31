5c94df903968eff27adeb391c6cc16c8d128e256
AM_Erizur <sajoudks@outlook.com>
2026-01-31 17:26:31 -0500

    Fix the window arrangement

--- Assets/Scripts/Runtime/ToggleWindow.cs
+++ Assets/Scripts/Runtime/ToggleWindow.cs
@@ -1,15 +1,43 @@
+using System.Linq;
 using UnityEngine;
 
 public class ToggleWindow : MonoBehaviour
 {
-    [SerializeField] private GameObject windowToToggle;
+    [SerializeField] private GameObject[] availableWindows;
+    private int lastWhich = 1;
 
-    public void ToggleWindowVisibility()
+    public void ToggleWindowVisibility(int which)
     {
-        if (windowToToggle != null)
+        if (which != 0)
         {
-            bool isActive = windowToToggle.activeSelf;
-            windowToToggle.SetActive(!isActive);
+            GameObject obj = availableWindows[which - 1];
+            RectTransform transform = obj.GetComponent<RectTransform>();
+
+            if (!obj.activeSelf)
+            {
+                obj.SetActive(true);
+                transform.SetAsLastSibling();
+            }
+            else
+            {
+                if (lastWhich == which)
+                {
+                    obj.SetActive(false);
+                    transform.SetAsFirstSibling();
+                } else
+                {
+                    if (transform.GetSiblingIndex() != availableWindows.Length - 1)
+                    {
+                        transform.SetAsLastSibling();
+                    } else
+                    {
+                        obj.SetActive(false);
+                        transform.SetAsFirstSibling();
+                    }
+                }
+            }
+
+            lastWhich = which;
         }
         else
         {
