eb2d0fbf0386a3116faed0ad46535ebcabe73321
Piero Barbieri <piero.barbieri50@gmail.com>
2026-01-31 16:44:56 -0500

    chat update

--- Assets/Scripts/Runtime/ChatTextController.cs
+++ Assets/Scripts/Runtime/ChatTextController.cs
@@ -3,27 +3,20 @@ using UnityEngine;
 
 public class ChatTextController : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI user;
-    [SerializeField] private TextMeshProUGUI message;
+    [SerializeField] private TextMeshProUGUI chatText;
     bool _banned = false;
-
-    private string chatText;
 
     public void SetChatText(string userText, string messageText)
     {
-        user.text = $"<b>{userText}: </b>";
-        message.text = messageText;
-        chatText = userText + messageText;
+        chatText.text = $"<b>{userText}:</b> {messageText}";
     }
 
     public void OnClick()
     {
         if (_banned) return;
-        
         _banned = true;
-        user.text = "<i>This user has been banned.</i>";
-        message.text = "";
-        Destroy(message.gameObject);
+
+        chatText.text = "<i>This message has been removed.</i>";
         //LayoutRebuilder.ForceRebuildLayoutImmediate(message.rectTransform);
     }
 }
